bac9f145ff5c23f2435479a19964b16424e97571
naturegecko <naturegecko@gmail.com>
2022-09-15 12:38:48 +0700

    Update progress

--- src/main/java/com/application/apis/MemberUserProfileAPIs.java
+++ src/main/java/com/application/apis/MemberUserProfileAPIs.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
