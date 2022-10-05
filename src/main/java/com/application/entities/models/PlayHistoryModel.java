7b97417ab60dea63e6722c0d9b04be0cdf030f7e
naturegecko <naturegecko@gmail.com>
2022-10-05 17:07:04 +0700

    History is no longer scream user detail.

--- src/main/java/com/application/entities/models/PlayHistoryModel.java
+++ src/main/java/com/application/entities/models/PlayHistoryModel.java
@@ -1,5 +1,6 @@
 package com.application.entities.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -30,8 +31,15 @@ public class PlayHistoryModel {
 	private String timestamp;
 
 	private int track_id;
+	
+	@Column(name = "account_id")
+	private int accountid;
+	
+	@ManyToOne
+	@JoinColumn(name = "track_id", referencedColumnName = "track_id",insertable = false,updatable = false)
+	private TracksModel track;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	/*@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "account_id", referencedColumnName = "account_id", insertable = false, updatable = false)
-	private UserAccountModel userAccount;
+	private UserAccountModel userAccount;*/
 }
